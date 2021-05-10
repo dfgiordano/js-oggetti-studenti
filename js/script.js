0d59cb18b8557812405ea5996ee4ca1d0b91a7a2
dfgiordano <federico.giordano90@gmail.com>
2021-05-10 16:31:03 +0200

    esercizio terminato

---
+++ js/script.js
@@ -0,0 +1,63 @@
+/* Creare un oggetto che descriva uno studente, con le seguenti proprietà: nome, cognome e età. */
+var studente = {
+    nome : "federico",
+    cognome : "giordano",
+    eta : 30
+};
+
+console.log(studente);
+
+/*  Stampare a schermo attraverso un ciclo for-in tutte le proprietà dell'oggetto. */
+for (var key in studente) {
+    console.log(studente[key]);
+}
+/* Creare un array di oggetti di studenti. */
+var studentiClasse34 = [
+    {
+        nome : "andrea",
+        cognome : "rossi",
+        eta : 50
+    },
+    {
+        nome : "paolo",
+        cognome : "blu",
+        eta : 35
+    },
+    {
+        nome : "manuel", 
+        cognome : "verde",
+        eta : 31
+    },
+    {
+        nome : "luca",
+        cognome : "giallo",
+        eta : 40
+    }
+];
+console.log(studentiClasse34);
+/*  Ciclare su tutti gli studenti e stampare per ognuno di essi, nome e cognome. */
+for (i = 0; i < studentiClasse34.length; i++) {
+    var studente = studentiClasse34[i];
+    console.log(studente.nome, studente.cognome);
+    /*
+    for (var key in studente) {
+        console.log(studente[key]);
+    }
+    */
+}
+
+/*  Dare la possibilità all'utente, attraverso 3 prompt(), di aggiungere un nuovo oggetto studente inserendo nell'ordine: nome, cognome e età. */
+
+var nome = prompt("Inserisci il nome del nuovo studente");
+var cognome = prompt("Inserisci il cognome del nuovo studente");
+var eta = parseInt(prompt("Inserisci l'età del nuovo studente"));
+var newObject = {};
+newObject.nome = nome;
+newObject.cognome = cognome;
+newObject.eta = eta;
+
+studentiClasse34.push(newObject);
+
+console.log(studentiClasse34);
+
+
